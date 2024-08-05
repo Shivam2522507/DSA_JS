af73ea1d9dfe05c30fff92080c20c6079e54bf25
Shivam Kumar Singh <shivamkumar12dhr@gmail.com>
2024-08-06 03:01:54 +0530

    bs With Iterative Approach

--- Searching/Binary Search with Iterative approach/bsWithIterativeApproach.js
+++ Searching/Binary Search with Iterative approach/bsWithIterativeApproach.js
@@ -1,10 +1,26 @@
-let array = [5,2,12,4,7,23,45,86,35,8]
+let array = [5, 7, 12, 41, 70, 230, 450, 826, 935, 8000];
 document.write("Given Array [" + array + "]");
-let find_item = false;
+// let find_item = false;
 // in iterative approach we use loop
-function find(val){
-   
-    if(!find_item){
-        console.warn("'" + val + "' is not Present in [" + array + "]")
+function find(val) {
+  let start = 0;
+  let end = array.length - 1;
+  let position = undefined;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (array[mid] === val) {
+      position = mid;
+      break;
+    } else if (array[mid] < val) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
     }
+  }
+  if (position != undefined) {
+    console.warn("' Your Value " + val + " is Present in " + position + " '");
+  } else {
+    console.warn("'" + val + "' is not Present in [" + array + "]");
+  }
 }
